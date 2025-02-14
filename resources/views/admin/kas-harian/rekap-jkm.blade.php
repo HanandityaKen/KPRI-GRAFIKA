e6dc21afe3bad0a56f588e7af09203a851fe36eb
aldila25 <aldilaangelina@gmail.com>
2025-02-14 21:23:25 +0700

    dila second commit

--- resources/views/admin/kas-harian/rekap-jkm.blade.php
+++ resources/views/admin/kas-harian/rekap-jkm.blade.php
@@ -38,7 +38,7 @@
   </div>
 
   <div class="flex justify-between items-center mb-6">
-    <h1 class="text-xl font-bold">Rekap JKM</h1>
+    <h1 class="text-xl font-bold">Rekap JKM Jurnal Kas Masuk</h1>
   </div>
 
   {{-- @livewire('rekap-jkm-filter') --}}

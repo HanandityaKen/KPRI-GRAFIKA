f10f4e0073c5da35c07a7e6d91a4ca7fd300e96d
Hananditya Kenanta Setyaputra <hanandityakenanta@gmail.com>
2025-05-18 19:57:08 +0700

    update-ubah py pada bg headline profile anggota

--- resources/views/anggota/profile.blade.php
+++ resources/views/anggota/profile.blade.php
@@ -10,7 +10,7 @@
 
             <!-- Header -->
             <header class="bg-green-100 text-green-700 w-full">
-            <div class="max-w-none px-6 py-16 md:py-20 lg:py-24 text-center">
+            <div class="max-w-none px-6 py-16 md:py-18 lg:py-18 text-center">
                 <h1 class="text-xl lg:text-2xl font-semibold">Profile</h1>
             </div>
             </header>

6948e4c428975b65edc59ad80cfcd1ad66099667
Hananditya Kenanta Setyaputra <hanandityakenanta@gmail.com>
2025-10-04 15:32:01 +0700

    update-menambahkan bintang merah di input nama di profile anggota

--- resources/views/livewire/anggota/form-profile.blade.php
+++ resources/views/livewire/anggota/form-profile.blade.php
@@ -2,8 +2,10 @@
     <div class="max-w-full mx-auto px-20">
         <h1 class="text-xl font-bold mb-4">Personal Data</h1>
             <div class="mb-3">
-                <label class="block mb-1 text-sm font-medium text-gray-900">Nama</label>
-                <input wire:model.live="nama" type="text" name="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2" value="{{ old('nama') }}"/>
+                <label class="block mb-1 text-sm font-medium text-gray-900">
+                    Nama <span class="text-red-500">*</span>
+                </label>
+                <input wire:model.live="nama" type="text" name="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2" value="{{ old('nama') }}" required/>
                 <p class="text-red-500 text-xs mt-1">{{ $error_nama }}</p>
             </div>
             <div class="mb-3">
